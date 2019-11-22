53edc4f74c156f5d46da6bb94871031d42b93b88
Your Name <you@example.com>
2019-11-22 11:43:57 -0500

    clear

---
+++ main.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const app = express();
+// app.listen(3000,()  => console.log("listening at 3000"))
+// app.use(express.static("server"))
+
+
+// app.post('api',(request,response) => {
+//     console.log(request)
+// })
